9cdf430bf0927e6b24496ce2245ac17d4c1978ea
Sushreeta Sahoo <sushreetas@geekyants.com>
2020-10-17 03:26:51 +0530

    chore(*): refactor code

--- src/api.ts
+++ src/api.ts
@@ -1,67 +1,78 @@
 import express, { NextFunction, Request, Response } from "express";
 export const app = express();
-import { createSubscription, listSubscription, cancelSubscription, cancelSubscriptionAtPeriodEnd } from "./billing";
+import {
+  createSubscription,
+  listSubscription,
+  cancelSubscription,
+  cancelSubscriptionAtPeriodEnd,
+  prorationSubscription,
+  prorationConfirmSubscription,
+} from "./billing";
 import { createPaymentIntent } from "./payment";
 import { createStripeCheckoutSEssion } from "./checkout";
 import { getOrCreateCustomer } from "./customer";
 
 app.use(express.json());
 
-//allow cross origin requests
+// allow cross origin requests
 import cors from "cors";
 import { handleStripeWebhook } from "./webhooks";
 import { createSetupIntent, listPaymentMethods } from "./customer";
 app.use(cors({ origin: true }));
 
-//sets rawBody for webhook handling
-app.use(express.json({
-  verify: (req, res, buffer) => (req["rawBody"] = buffer),
-
-}));
+// sets rawBody for webhook handling
+app.use(
+  express.json({
+    verify: (req, res, buffer) => (req["rawBody"] = buffer),
+  })
+);
 
 // catch async errors when awaiting promises
 const runAsync: Function = (callback: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
     callback(req, res, next).catch(next);
-  }
-}
+  };
+};
 
-// validate if user exist on the request
+// validate if user exist on the request [NOT USED PROPERLY FOR NOW, CAN BE USED AFTER AUTH IMPLEMENTATION]
 function validateUser(req: Request) {
-  const user = req['currentUser'];
+  const user = req["currentUser"];
   if (!user) {
     throw new Error(
-      'You must be logged in to make this request. i.e Authroization: Bearer <token>'
+      "You must be logged in to make this request. i.e Authroization: Bearer <token>"
     );
   }
-
   return user;
 }
 
 /*****************************
  * API *
-******************************/
+ ******************************/
 
-// check server health
+// Check server health
 app.get("/test", (req: Request, res: Response) => {
   res.status(200).send("API is up and running fine");
-})
+});
 
-// checkout
-app.post("/checkout", runAsync( async ({ body }: Request, res: Response) => {
-  res.send(
-    await createStripeCheckoutSEssion(body.line_items)
-  );
-}))
-
-// paymentIntent
-app.post("/payments", runAsync(async ({ body }: Request, res: Response) => {
-  res.send(await createPaymentIntent(body.amount));
-}));
-
-// billing & recurring subscriptions
+// Checkout payment
 app.post(
-  '/subscription',
+  "/checkout",
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createStripeCheckoutSEssion(body.line_items));
+  })
+);
+
+// PaymentIntent
+app.post(
+  "/payments",
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createPaymentIntent(body.amount));
+  })
+);
+
+// Billing & recurring payment (subscription)
+app.post(
+  "/subscription",
   runAsync(async (req: Request, res: Response) => {
     // const user = validateUser(req);
     const { userId, plan, payment_method } = req.body;
@@ -70,9 +81,9 @@ app.post(
   })
 );
 
-//list subscription
+// Show list of subscription
 app.get(
-  '/subscription',
+  "/subscription",
   runAsync(async (req: Request, res: Response) => {
     // const user = validateUser(req);
     const { userId } = req.body;
@@ -81,9 +92,9 @@ app.get(
   })
 );
 
-//cancel subscription immediately
+// Cancel subscription immediately
 app.patch(
-  '/subscription/cancel/:id',
+  "/subscription/cancel/:id",
   runAsync(async (req: Request, res: Response) => {
     // const user = validateUser(req);
     const { userId } = req.body;
@@ -92,20 +103,52 @@ app.patch(
   })
 );
 
-//cancel at period end subscription
+// Cancel at period end subscription
 app.patch(
-  '/subscription/cancelperiodend/:id',
+  "/subscription/cancel/periodend/:id",
   runAsync(async (req: Request, res: Response) => {
     // const user = validateUser(req);
     const { userId } = req.body;
-    const subscription = await cancelSubscriptionAtPeriodEnd(userId, req.params.id);
+    const subscription = await cancelSubscriptionAtPeriodEnd(
+      userId,
+      req.params.id
+    );
     res.send(subscription);
   })
 );
 
-//create customer
+// Ask confirmation for prorate of subscription (upgrade or downgrade of subscription)
 app.post(
-  '/customer',
+  "/subscription/prorate/:id",
+  runAsync(async (req: Request, res: Response) => {
+    // const user = validateUser(req);
+    const { plan, userId } = req.body;
+    const subscription = await prorationSubscription(
+      req.params.id,
+      plan,
+      userId
+    );
+    res.send(subscription);
+  })
+);
+
+// Final proration of subscription (upgrade or downgrade of subscription)
+app.patch(
+  "/subscription/prorate/confirm/:id",
+  runAsync(async (req: Request, res: Response) => {
+    // const user = validateUser(req);
+    const { plan } = req.body;
+    const subscription = await prorationConfirmSubscription(
+      req.params.id,
+      plan
+    );
+    res.send(subscription);
+  })
+);
+
+// Create customer
+app.post(
+  "/customer",
   runAsync(async (req: Request, res: Response) => {
     const { email } = req.body;
     const subscription = await getOrCreateCustomer(email);
@@ -113,20 +156,25 @@ app.post(
   })
 );
 
-// hook
+// Hook
 app.post("/hooks", runAsync(handleStripeWebhook));
 
-//save a card to a customer
-app.post("/wallet", runAsync(async (req: Request, res: Response) => {
-  const user = validateUser(req);
-  const setupIntent = await createSetupIntent(user.uid);
-  res.send(setupIntent);
-}))
+// Save a card to a customer
+app.post(
+  "/wallet",
+  runAsync(async (req: Request, res: Response) => {
+    const user = validateUser(req);
+    const setupIntent = await createSetupIntent(user.uid);
+    res.send(setupIntent);
+  })
+);
 
-// retrive all cards attached to a customer
-app.get("/wallet", runAsync(async (req: Request, res: Response) => {
-  const user = validateUser(req);
-
-  const wallet = await listPaymentMethods(user.uid);
-  res.send(wallet.data);
-}))
+// Retrive all cards attached to a customer
+app.get(
+  "/wallet",
+  runAsync(async (req: Request, res: Response) => {
+    const user = validateUser(req);
+    const wallet = await listPaymentMethods(user.uid);
+    res.send(wallet.data);
+  })
+);
--- src/billing.ts
+++ src/billing.ts
@@ -11,9 +11,11 @@ export const createSubscription = async (
   // const customer = await getOrCreateCustomer(userId);
 
   //attach the payment method to the customer
-  await stripe.paymentMethods.attach(payment_method, { customer: userId /*customer.id*/ });
+  await stripe.paymentMethods.attach(payment_method, {
+    customer: userId /*customer.id*/,
+  });
 
-  // set it as the default payment method 
+  // set it as the default payment method
   await stripe.customers.update(userId /*customer.id*/, {
     invoice_settings: { default_payment_method: payment_method },
   });
@@ -21,31 +23,23 @@ export const createSubscription = async (
   const subscription = await stripe.subscriptions.create({
     customer: userId /*customer.id*/,
     items: [{ plan }],
-    expand: ['latest_invoice.payment_intent'],
+    expand: ["latest_invoice.payment_intent"],
   });
 
   const invoice = subscription.latest_invoice as Stripe.Invoice;
   const payment_intent = invoice.payment_intent as Stripe.PaymentIntent;
 
   // update the user's status in your db
-  if (payment_intent.status === 'succeeded') {
-  //   await db
-  //     .collection('users')
-  //     .doc(userId)
-  //     .set({
-  //       stripeCustomerId: customer.id,
-  //       // activePlans: firestore.FieldValue.arrayUnion(plan),
-  //     },
-  //     { merge: true } //delete if any existing data in the document
-  //     );
+  if (payment_intent.status === "succeeded") {
+    //do something
   }
   return subscription;
-}
+};
 
 //cancel an active subscription
 export const cancelSubscription = async (
   userId: string,
-  subscriptionId: string,
+  subscriptionId: string
 ) => {
   //cancel the subscription immediately
   const subscription = await stripe.subscriptions.del(subscriptionId);
@@ -53,30 +47,79 @@ export const cancelSubscription = async (
   if (subscription.status == "canceled") {
     //update in db(remove the subscription plan)
   }
-  
+
   return subscription;
-}
+};
 export const cancelSubscriptionAtPeriodEnd = async (
   userId: string,
-  subscriptionId: string,
+  subscriptionId: string
 ) => {
-
   // canceled at the end of the current period
-  const subscription = await stripe.subscriptions.update(subscriptionId, { cancel_at_period_end: true });
+  const subscription = await stripe.subscriptions.update(subscriptionId, {
+    cancel_at_period_end: true,
+  });
 
   if (subscription.status == "canceled") {
     //update in db(remove the subscription plan)
   }
-  
+
   return subscription;
-}
+};
 
 export const listSubscription = async (userId: string) => {
   // const customer = await getOrCreateCustomer(userId);
   const subscriptions = await stripe.subscriptions.list({
     customer: userId, //customer.id,
-  })
-
+  });
   return subscriptions;
+};
 
-}
+export const prorationSubscription = async (
+  subId: string,
+  newPlan: string,
+  userId: string
+) => {
+  const subscription = await stripe.subscriptions.retrieve(subId);
+
+  // Set proration date to this moment:
+  const proration_date = Math.floor(Date.now() / 1000);
+
+  const items = [
+    {
+      id: subscription.items.data[0].id,
+      price: newPlan, // Switch to new price
+    },
+  ];
+
+  const invoice = await stripe.invoices.retrieveUpcoming({
+    customer: userId,
+    subscription: subId,
+    subscription_items: items,
+    subscription_proration_date: proration_date,
+  });
+
+  return invoice;
+};
+
+export const prorationConfirmSubscription = async (
+  subId: string,
+  newPlan: string
+) => {
+  const subscription = await stripe.subscriptions.retrieve(subId);
+  // Set proration date to this moment:
+  const proration_date = Math.floor(Date.now() / 1000);
+
+  stripe.subscriptions.update(subId, {
+    cancel_at_period_end: false,
+    proration_behavior: "create_prorations",
+    items: [
+      {
+        id: subscription.items.data[0].id,
+        price: newPlan,
+      },
+    ],
+    proration_date: proration_date,
+  });
+
+  return { success: true };
+};
